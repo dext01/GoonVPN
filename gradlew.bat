6ed126c3da791389f23273600f3a8c4c7e09b976
dext01 <130887490+dext01@users.noreply.github.com>
2026-05-26 01:50:05 +0700

    Delete gradlew.bat

--- gradlew.bat
+++
@@ -1,7 +0,0 @@
-@rem Gradle startup script for Windows
-@echo off
-setlocal
-set JAVA_HOME=C:\Program Files\Android\Android Studio\jbr
-set PATH=%JAVA_HOME%\bin;%PATH%
-set DIRNAME=%~dp0
-"%JAVA_HOME%\bin\java.exe" -classpath "%DIRNAME%gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
